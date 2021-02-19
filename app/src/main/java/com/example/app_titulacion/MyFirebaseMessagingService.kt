d9d41a6763c58651f101407cafe8c22b66074f77
modpuxep <rafaelbecerrabocangel@hotmail.com>
2021-02-19 16:50:46 -0500

    first commit 1.0

---
+++ app/src/main/java/com/example/app_titulacion/MyFirebaseMessagingService.kt
@@ -0,0 +1,19 @@
+package com.example.app_titulacion
+
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.google.firebase.messaging.ktx.remoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    //Notifiaciones en primer plano
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Looper.prepare()
+        Handler().post() {
+            Toast.makeText(baseContext, remoteMessage.notification?.title, Toast.LENGTH_LONG).show()
+        }
+    }
+}
